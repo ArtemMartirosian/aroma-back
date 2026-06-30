7275289851924b311ec409ad555779a52f0b250a
ArtemMartirosian <artem.martirosian2001@gmail.com>
2026-06-30 17:10:17 +0400

    Register admin seed entities in app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,11 +3,14 @@ import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AuthModule } from './auth/auth.module';
 import { BrandsModule } from './brands/brands.module';
+import { Brand } from './brands/entities/brand.entity';
 import { CatalogSeedService } from './catalog-seed.service';
 import { CategoriesModule } from './categories/categories.module';
+import { Category } from './categories/entities/category.entity';
 import { createDatabaseOptions } from './database.config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { Product } from './products/entities/product.entity';
 import { ProductsModule } from './products/products.module';
 
 @Module({
@@ -16,6 +19,7 @@ import { ProductsModule } from './products/products.module';
     TypeOrmModule.forRootAsync({
       useFactory: () => createDatabaseOptions(),
     }),
+    TypeOrmModule.forFeature([Brand, Category, Product]),
     AuthModule,
     ProductsModule,
     BrandsModule,
